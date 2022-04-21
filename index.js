2ea53b37b82c62b76139921dfd0394377e2855dc
Teewai57 <bobtoyoabasi@gmail.com>
2022-04-21 13:36:26 +0100

    changed to vue

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const jokeContainer = document.getElementById("joke");
-const btn = document.getElementById("btn");
-const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+// const jokeContainer = document.getElementById("joke");
+// const btn = document.getElementById("btn");
+// const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
-let getJoke = () => {
-    jokeContainer.classList.remove("fade");
-    fetch(url)
-    .then(data => data.json())
-    .then(item => {
-        jokeContainer.textContent = `${item.joke}`;
-        // jokeContainer.classList.add("fade");
-    });
-}
 
-btn.addEventListener("click", getJoke);
-getJoke();
+// let getJoke = () => {
+//     jokeContainer.classList.remove("fade");
+//     fetch(url)
+//     .then(data => data.json())
+//     .then(item => {
+//         jokeContainer.textContent = `${item.joke}`;
+//         // jokeContainer.classList.add("fade");
+//     });
+// }
+
+// btn.addEventListener("click", getJoke);
+// getJoke();
+
+const app = Vue.createApp({
+    data() {
+        return{
+            jokes: ""
+        }
+    },
+    methods: {
+        async getJokes(){
+            const res = await axios.get('https://icanhazdadjoke.com',{
+                headers:{
+                    Accept:'application/json',
+                }
+            });
+            const data = res.data.joke
+            console.log(data);
+            this.jokes = data
+
+        }
+    },
+    mounted(){
+
+    }
+})
+app.mount("#jokeApp");
